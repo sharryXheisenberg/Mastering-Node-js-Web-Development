c9b54700851687ff19fbf0de28a0d0da716d2102
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-10-17 10:20:16 +0530

    updated learning and changes

--- ch_4_Nodejs_concurrency/webapp/src/handler.ts
+++ ch_4_Nodejs_concurrency/webapp/src/handler.ts
@@ -31,15 +31,22 @@ const total = 2_000_000_000;
 const iterations = 5;
 let shared_counter = 0;
 
-export const handler = async (req:IncomingMessage, res:ServerResponse)=>{
- const request = shared_counter++;
- for(let iter = 0 ; iter<iterations;iter++){
-    for(let count = 0; count < total ; count ++){
-      count ++;
+export const handler = async (req:IncomingMessage,res:ServerResponse) =>{
+  const request = shared_counter++;
+
+  const iterate = async (iter:number=0)=>{
+    for(let count = 0;count<total;count++){
+      count++;
     }
-    const msg = `Request : ${request} , Iteration: ${(iter)}`;
+    const msg = `Request: ${request} , Iteration:${(iter)}`;
     console.log(msg);
+
     await writePromise.bind(res)(msg + "\n");
- }
- await endPromise.bind(res)("Done");
-}
+    if(iter == iterations -1){
+      await endPromise.bind(res)("Done");
+    }else{
+      setImmediate(()=>iterate(++iter));
+    }
+  }
+  iterate();
+};
